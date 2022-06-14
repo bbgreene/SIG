7c4c583cd68ae5d7be7c5988c3b3e05e4aba64a0
bbgreene <brian.greene.music@gmail.com>
2022-06-14 12:35:06 +0200

    included h file for pinknoise class

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "PinkNoise.h"
 
 //==============================================================================
 /**
@@ -85,45 +86,6 @@ private:
     
     
     //Pink noise class // from here https://www.firstpr.com.au/dsp/pink-noise/#Voss
-    class PinkNumber
-    {
-    private:
-      int max_key;
-      int key;
-      unsigned int white_values[5];
-      unsigned int range;
-    public:
-      PinkNumber(unsigned int range = 128)
-        {
-          max_key = 0x1f; // Five bits set
-          this->range = range;
-          key = 0;
-          for (int i = 0; i < 5; i++)
-     white_values[i] = rand() % (range/5);
-        }
-      int GetNextValue()
-        {
-          int last_key = key;
-          unsigned int sum;
-
-          key++;
-          if (key > max_key)
-     key = 0;
-          // Exclusive-Or previous value with current value. This gives
-          // a list of bits that have changed.
-          int diff = last_key ^ key;
-          sum = 0;
-          for (int i = 0; i < 5; i++)
-     {
-       // If bit changed get new random number for corresponding
-       // white_value
-       if (diff & (1 << i))
-         white_values[i] = rand() % (range/5);
-       sum += white_values[i];
-     }
-          return sum;
-        }
-    };
     
     //Pink noise object instantiation
     PinkNumber pn;
